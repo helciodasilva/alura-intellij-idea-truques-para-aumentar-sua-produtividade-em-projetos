4c07cb65b2ca4e16c283aef28b1b2635759598a3
helciodasilva <helciodasilva@gmail.com>
2020-11-05 21:40:37 -0300

    Aula 2: Navegando e refatorando o código

--- intellij-idea-aula-2/src/br/com/alura/bytebank/registrador/RegistroDePagamento.java
+++ intellij-idea-aula-2/src/br/com/alura/bytebank/registrador/RegistroDePagamento.java
@@ -13,21 +13,13 @@ public class RegistroDePagamento {
     public void registra(List<Pagamento> pagamentos) {
         for (int i = 0; i < pagamentos.size(); i++) {
             Pagamento pagamento = pagamentos.get(i);
-            if (pagamento.getTipo().equals(Tipo.CREDITO)) {
-                pagamento.setValor(pagamento.getValor() * 1.02);
-                PAGAMENTOS.add(pagamento);
-                System.out.println("Pagamento realizado " + pagamento);
-            }
-            if (pagamento.getTipo().equals(Tipo.DEBITO)) {
-                pagamento.setValor(pagamento.getValor() * 1.001);
-                PAGAMENTOS.add(pagamento);
-                System.out.println("Pagamento realizado " + pagamento);
-            }
-            if (pagamento.getTipo().equals(Tipo.DINHEIRO)) {
-                PAGAMENTOS.add(pagamento);
-                System.out.println("Pagamento realizado " + pagamento);
-            }
+            verificaTipo(pagamento);
+            salva(pagamento);
         }
+        exibeEfetuados();
+    }
+
+    private void exibeEfetuados() {
         if (!PAGAMENTOS.isEmpty()) {
             System.out.println("Todos os pagamentos");
             for (int i = 0; i < PAGAMENTOS.size(); i++) {
@@ -36,6 +28,20 @@ public class RegistroDePagamento {
         } else {
             System.out.println("Não temos pagamentos");
         }
+    }
+
+    private void verificaTipo(Pagamento pagamento) {
+        if (pagamento.getTipo().equals(Tipo.CREDITO)) {
+            pagamento.setValor(pagamento.getValor() * 1.02);
+        }
+        if (pagamento.getTipo().equals(Tipo.DEBITO)) {
+            pagamento.setValor(pagamento.getValor() * 1.001);
+        }
+    }
+
+    private void salva(Pagamento pagamento) {
+        PAGAMENTOS.add(pagamento);
+        System.out.println("Pagamento realizado " + pagamento);
     }
 
 }
