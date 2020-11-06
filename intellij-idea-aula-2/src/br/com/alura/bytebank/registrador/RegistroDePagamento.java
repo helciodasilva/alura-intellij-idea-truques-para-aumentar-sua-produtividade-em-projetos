bc3eb6769bf0fb30b8302661ef475e4e43760973
helciodasilva <helciodasilva@gmail.com>
2020-11-05 21:21:31 -0300

    Aula 2: Navegando e refatorando o código

--- intellij-idea-aula-2/src/br/com/alura/bytebank/registrador/RegistroDePagamento.java
+++ intellij-idea-aula-2/src/br/com/alura/bytebank/registrador/RegistroDePagamento.java
@@ -11,18 +11,31 @@ public class RegistroDePagamento {
     private final static List<Pagamento> psr = new ArrayList<>();
 
     public void registra(List<Pagamento> ps) {
-    for (int i = 0; i < ps.size(); i++) {
-        Pagamento p = ps.get(i);
-        if (p.getTipo().equals(Tipo.CREDITO)) { p.setValor(p.getValor() * 1.02);
-            psr.add(p); System.out.println("Pagamento realizado " + p);
-        } if (p.getTipo().equals(Tipo.DEBITO)) { p.setValor(p.getValor() * 1.001);
-            psr.add(p); System.out.println("Pagamento realizado " + p);
-        } if (p.getTipo().equals(Tipo.DINHEIRO)) { psr.add(p); System.out.println("Pagamento realizado " + p); }
-    } if (!psr.isEmpty()) { System.out.println("Todos os pagamentos");
-    for (int i = 0; i < psr.size(); i++) {
-        System.out.println(psr.get(i));
-    }
-    } else { System.out.println("Não temos pagamentos"); }
+        for (int i = 0; i < ps.size(); i++) {
+            Pagamento p = ps.get(i);
+            if (p.getTipo().equals(Tipo.CREDITO)) {
+                p.setValor(p.getValor() * 1.02);
+                psr.add(p);
+                System.out.println("Pagamento realizado " + p);
+            }
+            if (p.getTipo().equals(Tipo.DEBITO)) {
+                p.setValor(p.getValor() * 1.001);
+                psr.add(p);
+                System.out.println("Pagamento realizado " + p);
+            }
+            if (p.getTipo().equals(Tipo.DINHEIRO)) {
+                psr.add(p);
+                System.out.println("Pagamento realizado " + p);
+            }
+        }
+        if (!psr.isEmpty()) {
+            System.out.println("Todos os pagamentos");
+            for (int i = 0; i < psr.size(); i++) {
+                System.out.println(psr.get(i));
+            }
+        } else {
+            System.out.println("Não temos pagamentos");
+        }
     }
 
 }
